ba2220f4242d7642efe9cefedf851d319eb4b3ab
ShadowChild <shadychild12@gmail.com>
2016-03-10 14:31:11 +0000

    Removed files, switching to C#

--- src/main/java/io/github/shadowchild/growlithe/utils/Settings.java
+++
@@ -1,8 +0,0 @@
-package io.github.shadowchild.growlithe.utils;
-
-public class Settings {
-
-	public static boolean helpCommandActive;
-	public static boolean shutdownCommandActive;
-	public static boolean canRestart;
-}
